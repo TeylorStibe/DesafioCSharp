a7a29fe52c6310ff0737e8e81c7a1e11e2c01bd2
teylorstibe <stibe.teylor@gmail.com>
2021-03-05 18:18:51 -0300

    Primeiros passos

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int id{ get; set; }
+        public string Name{ get; set; }
+        public DateTime Birthdate{ get; set; }
+        public int CPF { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+    }
+}
